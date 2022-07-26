dbbe010b954a27da3a6a4af2f7ec7a848770acba
matheushajer <matheushajer@gmail.com>
2022-07-26 20:37:56 -0300

    Endpoint Post category  (insert)

--- backend/src/main/java/br/com/matheushajer/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/br/com/matheushajer/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package br.com.matheushajer.dscatalog.resources;
 
+import java.net.URI;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.matheushajer.dscatalog.dto.CategoryDTO;
 import br.com.matheushajer.dscatalog.services.CategoryService;
@@ -27,6 +31,12 @@ public class CategoryResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(service.insert(dto));
 	}
 
 }
--- backend/src/main/java/br/com/matheushajer/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/matheushajer/dscatalog/services/CategoryService.java
@@ -30,4 +30,12 @@ public class CategoryService {
 		return new CategoryDTO(optional.orElseThrow(() -> new EntityNotFoundException("ID não encontrado")));
 	}
 
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
 }
