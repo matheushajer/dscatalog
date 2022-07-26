037e416f9010d133612ae7836b03ed35587a0d7f
matheushajer <matheushajer@gmail.com>
2022-07-26 17:59:17 -0300

    Teste de seeding para o database

--- backend/src/main/java/br/com/matheushajer/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/matheushajer/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.matheushajer.dscatalog.entities.Category;
 import br.com.matheushajer.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public Collection<Category> findall(){
 		return repository.findAll();
 		
