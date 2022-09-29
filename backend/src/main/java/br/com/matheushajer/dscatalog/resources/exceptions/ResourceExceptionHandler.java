e65d0e5c5f4538ed8efd63c402c6a5f5a68f9fc4
matheushajer <matheushajer@gmail.com>
2022-09-29 18:39:13 -0300

    Tratamento de erros de validação com resposta.

--- backend/src/main/java/br/com/matheushajer/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/matheushajer/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -41,5 +43,21 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException exception, HttpServletRequest request){
+		ValidationError err = new ValidationError();
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		err.setError("Validation exception");
+		err.setMessage(exception.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for(FieldError f :  exception.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+	}
 	
 }
